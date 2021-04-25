e5dcf6ed7f539d0151ef3d53a473c9d6d3ac5996
halweg <halweg@126.com>
2021-04-25 15:41:29 +0800

    重构方法 ： 提炼变量 范例2 重构后

--- 01refactor_list/03提炼变量/demo02.js
+++ 01refactor_list/03提炼变量/demo02.js
@@ -9,11 +9,22 @@ class Order {
     get itemPrice() {return this._data.itemPrice;}
 
     get price() {
-        return this.quantity * this.itemPrice -
-            Math.max(0, this.quantity - 500) * this.itemPrice * 0.05 +
-            Math.min(this.quantity * this.itemPrice * 0.1, 100);
+        return this.basePrice - this.quantityDiscount + this.shippingFee;
+    }
+
+    get basePrice () {
+        return this.quantity * this.itemPrice
+    }
+
+    get quantityDiscount() {
+        return Math.max(0, this.quantity - 500) * this.itemPrice * 0.05;
+    }
+
+    get shippingFee() {
+        return Math.min(this.quantity * this.itemPrice * 0.1, 100);
     }
 }
+
 
 let order = new Order({
     quantity : 2,
