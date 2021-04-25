bbffce3efc83ca78080c5b701465905711ecbae9
halweg <halweg@126.com>
2021-04-25 15:35:19 +0800

    重构方法：提炼变量 范例1重构后

--- 01refactor_list/03提炼变量/demo01.js
+++ 01refactor_list/03提炼变量/demo01.js
@@ -3,12 +3,16 @@ let order = {
     itemPrice : 700,
 }
 
-//优惠，quantity超过500的部分打折 0.05 , 额外收取0.1份额的费用（上限100元）
+//优惠，quantity超过500的部分打折 0.05 , 额外收取0.1份额的费用（运费，上限100元）
 function price(order) {
-//price is base price - quantity discount + shipping
-    return order.quantity * order.itemPrice -
-        Math.max(0, order.quantity - 500) * order.itemPrice * 0.05 +
-        Math.min(order.quantity * order.itemPrice * 0.1, 100);
+    //price is base price - quantity discount + shipping
+    const basePrice = order.quantity * order.itemPrice;
+
+    const quantityDiscount = Math.max(0, order.quantity - 500) * order.itemPrice * 0.05;
+
+    const shippingFee = Math.min(basePrice * 0.1, 100);
+
+    return basePrice - quantityDiscount + shippingFee;
 }
 
 console.log(price(order));
