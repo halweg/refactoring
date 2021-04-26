defc7aed8a5e17404ce76c97c7f74caee9b76b27
halweg <halweg@126.com>
2021-04-26 11:08:42 +0800

    重构方法：封装变量 范例1 重构后

--- 01refactor_list/05封装变量/demo01.js
+++ 01refactor_list/05封装变量/demo01.js
@@ -2,9 +2,20 @@ let defaultOwner = {firstName: "Martin", lastName: "Fowler"};
 
 //假设省略一些复杂的业务流程
 //在这里修改了defaultOwner的值
-defaultOwner = {firstName: "Rebecca", lastName: "Parsons"};
+setDefaultOwner({firstName: "Rebecca", lastName: "Parsons"})
+
+//首先做一个基础封装
+function getDefaultOwner()
+{
+    return defaultOwner;
+}
+
+function setDefaultOwner(arg)
+{
+    defaultOwner = arg;
+}
 
 //使用defaultOwner的代码平平无奇
 let spaceship = {};
-spaceship.owner = defaultOwner;
+spaceship.owner = getDefaultOwner();
 console.log(spaceship);
