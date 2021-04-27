f1a1eb3bafd870c1f6cb2b1ef659f253c95bc953
halweg <halweg@126.com>
2021-04-27 11:27:04 +0800

    重构方法：拆分阶段 范例1 重构后

--- 01refactor_list/09拆分阶段/demo01.js
+++ 01refactor_list/09拆分阶段/demo01.js
@@ -2,14 +2,22 @@ const productModel = require('./product');
 const shippingMethodModel = require('./shippingMethod');
 
 function priceOrder(product, quantity, shippingMethod) {
+    const priceData = calculatePricingData(product, quantity);
+    return  applyShipping(priceData, shippingMethod);
+}
+
+function calculatePricingData(product, quantity) {
     const basePrice = product.basePrice * quantity;
     const discount = Math.max(quantity - product.discountThreshold, 0)
         * product.basePrice * product.discountRate;
-    const shippingPerCase = (basePrice > shippingMethod.discountThreshold)
+    return { basePrice : basePrice, quantity:quantity, discount: discount};
+}
+
+function applyShipping(priceData, shippingMethod) {
+    const shippingPerCase = (priceData.basePrice > shippingMethod.discountThreshold)
         ? shippingMethod.discountedFee : shippingMethod.feePerCase;
-    const shippingCost = quantity * shippingPerCase;
-    const price = basePrice - discount + shippingCost;
-    return price;
+    const shippingCost = priceData.quantity * shippingPerCase;
+    return priceData.basePrice - priceData.discount + shippingCost;
 }
 
 let product = productModel.newProduct(1090, "华为 P1080", 0.9);
