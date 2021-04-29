42754a5aede0a47f9f21d7931906ea438840b8b3
halweg <halweg@126.com>
2021-04-29 15:26:34 +0800

    重构方法：封装集合 范例1 重构后

--- 02encapsulation/02封装集合/demo01.js
+++ 02encapsulation/02封装集合/demo01.js
@@ -3,15 +3,7 @@ const Course = require('./course')
 
 let peter = Person.createPerson("皮特");
 
-let mathCourse    = Course.createCourse("math", false);
-let foreignCourse = Course.createCourse("english", false);
-let scienceCourse = Course.createCourse("science", true);
-let peterCourse = [];
-
-peterCourse.push(mathCourse);
-peterCourse.push(foreignCourse);
-peterCourse.push(scienceCourse);
-
-peter.courses = peterCourse;
-
+peter.addCourse(Course.createCourse("数学课", false));
+peter.addCourse(Course.createCourse("语文课", false));
+peter.addCourse(Course.createCourse("科学课", true));
 console.log(peter);
