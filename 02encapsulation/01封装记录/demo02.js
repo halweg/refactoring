a91905ef5a34d3f0f042e8a484b460782375b103
halweg <halweg@126.com>
2021-04-29 13:50:16 +0800

    重构方法：封装记录 范例2 重构后

--- 02encapsulation/01封装记录/demo02.js
+++ 02encapsulation/01封装记录/demo02.js
@@ -1,15 +1,39 @@
 //范例2： 封装嵌套记录
-const customerData = require('./_data_customer').customer
-
+let customers = require('./_data_customer').customer
 
 //customerData[customerID].usages[year][month] = amount;    修改的例子
+
+//zz_getRawDataOfCustomers()[customerID].usages[year][month] = amount; 修改的例子重构后
+
 //读取的例子
 function compareUsage (customerID, laterYear, month) {
-    const later = customerData[customerID].usages[laterYear][month];
-    const earlier = customerData[customerID].usages[laterYear - 1][month];
+    const later = getRawDataOfCustomers()[customerID].usages[laterYear][month];
+    const earlier = getRawDataOfCustomers()[customerID].usages[laterYear - 1][month];
     return {laterAmount: later, change: later - earlier};
 }
 
+function getRawDataOfCustomers() {
+    return customers;
+}
 
+function setRawDataOfCustomers(arg) {
+    customers = arg;
+}
 
-console.log(customerData)
+class CustomersData {
+    constructor(data) {
+        this._data = data;
+    }
+
+    getRawDataOfCustomers() {
+        return this._data;
+    }
+
+    setUsage(customerID, year, month, amount) {
+        this._data[customerID].usages[year][month] = amount;
+    }
+}
+
+const customersData = new CustomersData(customers);
+
+console.log(customersData.getRawDataOfCustomers())
