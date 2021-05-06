9d8efb870ccb01a105bec699a564d69d0c686f2d
halweg <halweg@126.com>
2021-05-06 23:38:02 +0800

    重构手法： 以查询取代临时变量 范例1 重构后

--- 02encapsulation/04以查询取代临时变量/order.js
+++ 02encapsulation/04以查询取代临时变量/order.js
@@ -6,10 +6,20 @@ class Order {
     }
 
     get price() {
-        let basePrice = this._quantity * this._item.price;
+        //let basePrice = this._quantity * this._item.price;
+        //const basePrice = this._quantity * this._item.price; 重构第一步，先把它改成不可变，再测试，确定有没有被从新腹直过
+        return this.basePrice * this.discountFactor;
+    }
+
+    get basePrice() {
+        return this._quantity * this._item.price;
+    }
+
+    get discountFactor() {
         let discountFactor = 0.98;
-        if (basePrice > 1000) discountFactor -= 0.03;
-        return basePrice * discountFactor;
+        if (this.basePrice > 1000)
+            discountFactor -= 0.03;
+        return discountFactor;
     }
 }
 
